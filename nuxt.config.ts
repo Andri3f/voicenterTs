91536c6b404c827d0f36d5550802598dc8258a67
Andri Kocherha <kotschergaandrij@gmail.com>
2025-01-11 01:08:36 +0200

    adad port

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
 	devtools: { enabled: true },
 	modules: ["@pinia/nuxt"],
 	plugins: ["@/plugins/screenWidthClass.client.js", "@/plugins/fontAwesome.js"],
-
 	css: ["@/assets/styles/reset/index.css", "@/assets/styles/common/commonStyle.css"],
+	server: {
+		port: 3000, // Вказуємо номер порту
+		host: "0.0.0.0", // Для доступу додатку через мережу
+	},
 })
